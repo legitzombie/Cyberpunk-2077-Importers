14b30f318697c6d00835e1c2cfbe898886d8c3e3
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-07 17:40:14 -0400

    Add files via upload

---
+++ Image Replacers/CustomWallpapers/Utils/RENAMEIMAGE.bat
@@ -0,0 +1,59 @@
+@echo off
+
+REM  ▄▄▄██▀▀▀▄▄▄       ███▄ ▄███▓▓█████   ██████
+REM    ▒██  ▒████▄    ▓██▒▀█▀ ██▒▓█   ▀ ▒██    ▒
+REM    ░██  ▒██  ▀█▄  ▓██    ▓██░▒███   ░ ▓██▄
+REM ▓██▄██▓ ░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄   ▒   ██▒
+REM  ▓███▒   ▓█   ▓██▒▒██▒   ░██▒░▒████▒▒██████▒▒
+REM  ▒▓▒▒░   ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░
+REM  ▒ ░▒░    ▒   ▒▒ ░░  ░      ░ ░ ░  ░░ ░▒  ░ ░
+REM  ░ ░ ░    ░   ▒   ░      ░      ░   ░  ░  ░
+REM  ░   ░        ░  ░       ░      ░  ░      ░
+REM
+REM          ▄▄▄█████▓ ██░ ██ ▓█████
+REM          ▓  ██▒ ▓▒▓██░ ██▒▓█   ▀
+REM          ▒ ▓██░ ▒░▒██▀▀██░▒███
+REM          ░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄
+REM            ▒██▒ ░ ░▓█▒░██▓░▒████▒
+REM            ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░
+REM              ░     ▒ ░▒░ ░ ░ ░  ░
+REM            ░       ░  ░░ ░   ░
+REM                    ░  ░  ░   ░  ░
+REM
+REM  ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███
+REM ▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
+REM ▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒
+REM ░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄
+REM ░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒
+REM  ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
+REM  ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
+REM  ░  ░░ ░  ░   ▒   ░        ░ ░░ ░    ░     ░░   ░
+REM  ░  ░  ░      ░  ░░ ░      ░  ░      ░  ░   ░
+REM  
+
+setlocal enabledelayedexpansion
+
+set "script_path=%~dp0"
+
+for %%i in ("%script_path%\..") do set "source_path=%%~fi\Image"
+for %%i in ("%script_path%\..") do set "dest_path=%%~fi\temp\images"
+
+if not exist "%dest_path%" mkdir "%dest_path%"
+
+set count=0
+
+for %%f in ("%source_path%\*") do (
+    set "ext=%%~xf"
+    copy "%%f" "%dest_path%\wallpaper_arasaka!ext!" >nul
+    copy "%%f" "%dest_path%\wallpaper_generic-1!ext!" >nul
+    copy "%%f" "%dest_path%\wallpaper_generic!ext!" >nul
+    copy "%%f" "%dest_path%\wallpaper_maelstrom!ext!" >nul
+    copy "%%f" "%dest_path%\wallpaper_militech!ext!" >nul
+    copy "%%f" "%dest_path%\wallpaper_konpeki!ext!" >nul
+    set /a count+=1
+)
+
+endlocal
+
+
+
