824e50a7cd0964f91b7bb9357a8089a34e240d7b
JamesTheHacker <56886589+legitzombie@users.noreply.github.com>
2025-08-09 17:45:02 -0400

    Add files via upload

---
+++ Image Replacers/Custom_Giant_Commercial_B/Run.bat
@@ -0,0 +1,78 @@
+@echo off
+
+REM  ▄▄▄██▀▀▀▄▄▄       ███▄ ▄███▓▓█████   ██████
+REM    ▒██  ▒████▄    ▓██▒▀█▀ ██▒▓█   ▀ ▒██    ▒
+REM    ░██  ▒██  ▀█▄  ▓██    ▓██░▒███   ░ ▓██▄
+REM ▓██▄██▓ ░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄   ▒   ██▒
+REM  ▓███▒   ▓█   ▓██▒▒██▒   ░██▒░▒████▒▒██████▒▒
+REM  ▒▓▒▒░   ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░
+REM  ▒ ░▒░    ▒   ▒▒ ░░  ░      ░ ░ ░  ░░ ░▒  ░ ░
+REM  ░ ░ ░    ░   ▒   ░      ░      ░   ░  ░  ░
+REM  ░   ░        ░  ░       ░      ░  ░      ░
+REM
+REM          ▄▄▄█████▓ ██░ ██ ▓█████
+REM          ▓  ██▒ ▓▒▓██░ ██▒▓█   ▀
+REM          ▒ ▓██░ ▒░▒██▀▀██░▒███
+REM          ░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄
+REM            ▒██▒ ░ ░▓█▒░██▓░▒████▒
+REM            ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░
+REM              ░     ▒ ░▒░ ░ ░ ░  ░
+REM            ░       ░  ░░ ░   ░
+REM                    ░  ░  ░   ░  ░
+REM
+REM  ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███
+REM ▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
+REM ▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒
+REM ░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄
+REM ░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒
+REM  ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
+REM  ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
+REM  ░  ░░ ░  ░   ▒   ░        ░ ░░ ░    ░     ░░   ░
+REM  ░  ░  ░      ░  ░░ ░      ░  ░      ░  ░   ░
+REM  
+
+setlocal
+
+color 0D
+set "PATH=%SystemRoot%\System32;%PATH%"
+
+call Utils/GETHELPERS.bat
+
+call Utils/GETREDIST.bat
+
+call Utils/GETDOTNET.bat
+
+if %errorlevel%==123 exit
+
+call Utils/GETPYTHON.bat
+
+if %errorlevel%==123 exit
+
+call Utils/GETPIP.bat
+
+call Utils/GETPILLOW.bat
+
+call Utils/GETREQUESTS.bat
+
+call Utils/GETCLI.bat
+
+call Utils/CREATEFOLDERS.bat
+
+call Utils/RENAMEIMAGE.bat
+
+call Utils/GETATLASGENERATOR.bat
+
+python.exe Utils/generate_inkatlas.py
+
+call Utils/JSON2INKATLAS.bat
+
+call Utils/PNG2XBM.bat 
+
+call Utils/ARCHIVE.bat
+
+call Utils/CLEANUP.bat
+
+call Utils/FINDGAME.bat
+
+
+endlocal
